aed6d569939ae632b8d73e486599baf47f21307a
srouet <simon.rouet@inra.fr>
2019-08-05 14:38:09 +0200

    Remplacement des fonctions Sirius de vernalisation et induction secondaire par de nouvelles fonctions

--- lgrass/flowering_functions.py
+++ lgrass/flowering_functions.py
@@ -13,41 +13,57 @@ class FloweringFunctions:
         :return:
         """
         if self.param.temp_vern_min <= temperature <= self.param.temp_vern_inter:
-            primary_induction_increment = self.param.vai * temperature + self.param.vbee
+            primary_induction_increment = self.param.daily_vern_rate * temperature + self.param.basic_vern_rate
         elif self.param.temp_vern_inter < temperature <= self.param.temp_vern_max:
-            primary_induction_increment = (self.param.vai * temperature + self.param.vbee) * (self.param.temp_vern_max - temperature) / (self.param.temp_vern_max - self.param.temp_vern_inter)
+            primary_induction_increment = (self.param.daily_vern_rate * temperature + self.param.basic_vern_rate) * (self.param.temp_vern_max - temperature) / (self.param.temp_vern_max - self.param.temp_vern_inter)
         else:
             primary_induction_increment = 0
         return primary_induction_increment
 
-
-    def approx_final_leaf_number_calculation(self, daylength, potential_leaf_number):
+    def photoperiod_induction_function(self, daylength):
         """
 
         :param daylength:
-        :param potential_leaf_number:
         :return:
         """
-        approx_final_leaf_number = min(potential_leaf_number, potential_leaf_number + self.param.sldl * (self.param.saturation_daylength - daylength))
-        return approx_final_leaf_number
+        if daylength < self.param.photoperiod_min:
+            secondary_induction_increment = 0
+        else:
+            secondary_induction_increment = (daylength - self.param.photoperiod_min) / (self.param.photoperiod_max - self.param.photoperiod_min)
+        return secondary_induction_increment
+
+#    def final_phytomer_number
 
 
-    def potential_leaf_number_calculation(self, primary_induction_index):
-        """
 
-        :self.param primary_induction_index:
-        :return:
-        """
-        potential_leaf_number = self.param.absolute_max_leaf_number - (self.param.absolute_max_leaf_number - self.param.absolute_min_leaf_number) * primary_induction_index
-        return potential_leaf_number
-
-    def test_vernalisation(self, primary_induction_index, potential_leaf_number, primordia_number):
-        """
-
-        :self.param primary_induction_index:
-        :self.param potential_leaf_number:
-        :self.param primordia_number:
-        :return:
-        """
-        if(primary_induction_index == 1 or potential_leaf_number == self.param.absolute_min_leaf_number or potential_leaf_number <= primordia_number):
-            return True
+    #
+    # def approx_final_leaf_number_calculation(self, daylength, potential_leaf_number):
+    #     """
+    #
+    #     :param daylength:
+    #     :param potential_leaf_number:
+    #     :return:
+    #     """
+    #     approx_final_leaf_number = min(potential_leaf_number, potential_leaf_number + self.param.sldl * (self.param.saturation_daylength - daylength))
+    #     return approx_final_leaf_number
+    #
+    #
+    # def potential_leaf_number_calculation(self, primary_induction_index):
+    #     """
+    #
+    #     :self.param primary_induction_index:
+    #     :return:
+    #     """
+    #     potential_leaf_number = self.param.absolute_max_leaf_number - (self.param.absolute_max_leaf_number - self.param.absolute_min_leaf_number) * primary_induction_index
+    #     return potential_leaf_number
+    #
+    # def test_vernalisation(self, primary_induction_index, potential_leaf_number, primordia_number):
+    #     """
+    #
+    #     :self.param primary_induction_index:
+    #     :self.param potential_leaf_number:
+    #     :self.param primordia_number:
+    #     :return:
+    #     """
+    #     if(primary_induction_index == 1 or potential_leaf_number == self.param.absolute_min_leaf_number or potential_leaf_number <= primordia_number):
+    #         return True
