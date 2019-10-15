ef241436d5b7d4ca3c4e067a232c230951521b5c
rbarillot <romain.barillot@inra.fr>
2019-10-15 17:24:56 +0200

    Fix bug [#12151]: fichier surface_biomass.csv comporte maintenant les surfaces et biomasses des feuilles émergées à l'échelle des talles.

--- test/test_lgrass.py
+++ test/test_lgrass.py
@@ -44,7 +44,7 @@ DESIRED_SORTIE_SIMULVALIDCOUPE_FILENAME = 'desired_sortie_simul_ValidCoupe_50_{}
 DESIRED_SORTIE_SURFACE_BIOMASS_FILENAME = 'desired_surface_biomass_50_{}_30.csv'.format(NSTEP)
 
 
-def compare_actual_to_desired(data_dirpath, actual_data_df, desired_data_filename, actual_data_filename=None):
+def compare_actual_to_desired(data_dirpath, actual_data_df, desired_data_filename, actual_data_filename=None, overwrite_desired_data=False):
     # read desired data
     desired_data_filepath = os.path.join(data_dirpath, desired_data_filename)
     desired_data_df = pd.read_csv(desired_data_filepath)
@@ -53,17 +53,21 @@ def compare_actual_to_desired(data_dirpath, actual_data_df, desired_data_filenam
         actual_data_filepath = actual_data_filename
         actual_data_df.to_csv(actual_data_filepath, na_rep='NA', index=False)
 
-    # keep only numerical data
-    for column in ('nb_feuillemergees', 'Phase'):
-        if column in desired_data_df.columns:
-            del desired_data_df[column]
-            del actual_data_df[column]
+    if overwrite_desired_data:
+        desired_data_filepath = os.path.join(data_dirpath, desired_data_filename)
+        actual_data_df.to_csv(desired_data_filepath, na_rep='NA', index=False)
+    else:
+        # keep only numerical data
+        for column in ('nb_feuillemergees', 'Phase'):
+            if column in desired_data_df.columns:
+                del desired_data_df[column]
+                del actual_data_df[column]
 
-    # compare to the desired data
-    np.testing.assert_allclose(actual_data_df.values, desired_data_df.values, RELATIVE_TOLERANCE, ABSOLUTE_TOLERANCE)
+        # compare to the desired data
+        np.testing.assert_allclose(actual_data_df.values, desired_data_df.values, RELATIVE_TOLERANCE, ABSOLUTE_TOLERANCE)
 
 
-def test_run():
+def test_run(overwrite_desired_data=False):
     lpy_filename = os.path.join(lgrass.__path__[0], "lgrass.lpy")
     lsys = Lsystem(lpy_filename)
     axiom = lsys.axiom
@@ -78,13 +82,11 @@ def test_run():
     evol = pd.read_csv(lsys.chemin_fichier3.name)
 
     # # compare outputs
-    compare_actual_to_desired(OUTPUTS_DIRPATH, surface_biomass, DESIRED_SORTIE_SURFACE_BIOMASS_FILENAME, lsys.chemin_fichier1.name)
-    compare_actual_to_desired(OUTPUTS_DIRPATH, sortie_simul_ValidCoupe, DESIRED_SORTIE_SIMULVALIDCOUPE_FILENAME, lsys.chemin_fichier2.name)
-    compare_actual_to_desired(OUTPUTS_DIRPATH, evol, DESIRED_SERIE_FOLIAIRE_FILENAME, lsys.chemin_fichier3.name)
+    compare_actual_to_desired(OUTPUTS_DIRPATH, surface_biomass, DESIRED_SORTIE_SURFACE_BIOMASS_FILENAME, lsys.chemin_fichier1.name, overwrite_desired_data)
+    compare_actual_to_desired(OUTPUTS_DIRPATH, sortie_simul_ValidCoupe, DESIRED_SORTIE_SIMULVALIDCOUPE_FILENAME, lsys.chemin_fichier2.name, overwrite_desired_data)
+    compare_actual_to_desired(OUTPUTS_DIRPATH, evol, DESIRED_SERIE_FOLIAIRE_FILENAME, lsys.chemin_fichier3.name, overwrite_desired_data)
 
     print "Test passed successfully"
 
 if __name__ == '__main__':
-    # import timeit
-    # print(timeit.timeit("test_run()", setup="from __main__ import test_run", number=100))
-    test_run()
+    test_run(overwrite_desired_data=False)
