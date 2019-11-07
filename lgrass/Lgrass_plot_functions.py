f53ed66a4e07aaba4e0a0deec5ef999636e73412
rbarillot <romain.barillot@inra.fr>
2019-11-07 15:40:59 +0100

    - correction unités sorties caribu pour calcul RUE. Pour activer cette option, il faudra ajouter le PAR incident dans le fichier meteo. - renommage de certaines variables pour etre plus cohérent (Surfoliairetotale -> surface_foliaire_emergee, Feuille.surface -> Feuille.surface_limbe + Feuille.surface_gaine) [T5407] - ajout d'un repertoire 'graphs' pour stocker les differents graphs produits et ajout nouveau graph (LAI, nb talles, nb feuilles)

--- lgrass/Lgrass_plot_functions.py
+++ lgrass/Lgrass_plot_functions.py
@@ -1,17 +1,20 @@
-# coding: utf8
+# -*- coding: latin-1 -*-
+
 import matplotlib.pyplot as plt
 import matplotlib.backends.backend_pdf
 import numpy as np
 import pandas as pd
 import math
+import os
 
 
 class GraphicOutputs:
-    def __init__(self):
+    def __init__(self, graph_dirpath):
+        self.graph_dirpath = graph_dirpath
         self.color_list = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
 
-    def graph_induction(self, path, csv_name):
-        df = pd.read_csv(path + csv_name + '.csv')
+    def graph_induction(self, csv_name):
+        df = pd.read_csv(csv_name + '.csv')
         plt.ioff()
         plt.figure()
         number_of_tiller = float(len(set(df.Id_talle.tolist())))
@@ -22,12 +25,12 @@ class GraphicOutputs:
             plt.ylabel(u'Induction rate (%)')
             plt.plot(u'Day', u'Vernalisation_rate', data=df[(df.Id_talle == tiller)], color=col, linestyle='-')
             plt.plot(u'Day', u'Secondary_induction_rate', data=df[(df.Id_talle == tiller)], color=col, linestyle=':')
-        plt.savefig(path + csv_name + '.pdf')
+        plt.savefig(os.path.join(self.graph_dirpath, 'induction' + '.pdf'))
         plt.close()
 
-    def graph_length(self, path, csv_name):
-        pdf_file = matplotlib.backends.backend_pdf.PdfPages(path + csv_name + '.pdf')
-        df = pd.read_csv(path + csv_name + '.csv')
+    def graph_length(self, csv_name):
+        pdf_file = matplotlib.backends.backend_pdf.PdfPages(os.path.join(self.graph_dirpath, 'longueur_feuilles' + '.pdf'))
+        df = pd.read_csv(csv_name + '.csv')
         number_of_tiller = len(np.unique(df.Id_talle.tolist()))
         i = 0
         for tiller in set(df.Id_talle.tolist()):
@@ -51,6 +54,57 @@ class GraphicOutputs:
             plt.close()
         pdf_file.close()
 
+    def graph_tiller_number(self, csv_name):
+        plt.figure()
 
-graph = GraphicOutputs()
-graph.graph_length(r'outputs', '\output_organ_lengths')
+        # Read csv file and group by plant
+        df = pd.read_csv(csv_name)
+        df_grouped = df.groupby('id plante')
+
+        # Plot tiller number per plant
+        for id_plant, data in df_grouped:
+            data.groupby('TPS')['id talle'].count().plot(label=id_plant, legend=True)
+
+        # Legends
+        plt.xlabel(u'Temps thermique (°C jour-1)')
+        plt.ylabel(u'Nombre de talles par n° plante')
+        # Save plot
+        plt.savefig(os.path.join(self.graph_dirpath, 'Nombre_talles' + '.PNG'))
+        plt.close()
+
+    def graph_LAI(self, csv_name, pattern):
+        plt.figure()
+        plt.ioff()
+
+        # Read csv file
+        df = pd.read_csv(csv_name)
+        # plot LAI
+        surface_pattern = pattern**2
+        df['LAI'] = df['Surface_feuilles_emergees'] / surface_pattern
+        df.groupby('TPS')['LAI'].sum().plot()
+
+        # Legends
+        plt.xlabel(u'Temps thermique (°C jour-1)')
+        plt.ylabel('LAI')
+        # Save plot
+        plt.savefig(os.path.join(self.graph_dirpath, 'LAI' + '.PNG'))
+        plt.close()
+
+    def graph_leaf_number(self, csv_name):
+        pdf_file = matplotlib.backends.backend_pdf.PdfPages(os.path.join(self.graph_dirpath, 'Nombre_feuilles' + '.pdf'))
+
+        # Read csv file
+        df = pd.read_csv(csv_name)
+        df_grouped = df.groupby('topologie')
+
+        # plot leaf number
+        for topo, data in df_grouped:
+            plt.figure()
+            data.groupby('TPS')['nb_feuille_emergees'].mean().plot(label=topo, legend=True)
+            # Legends
+            plt.xlabel(u'Temps thermique (°C jour-1)')
+            plt.ylabel('Leaf number per axis')
+            # Save plot
+            pdf_file.savefig()
+            plt.close()
+        pdf_file.close()
