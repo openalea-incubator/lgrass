cecceabfc2e81da5688471b9d76b7eb287daf5ac
Thibault <thibault.raquet@inrae.fr>
2020-02-24 17:28:03 +0100

    Adding new branch replace geno by id_geno and paramPlante and genotypes separated

--- lgrass/l-grass_batch_flowering.py
+++ lgrass/l-grass_batch_flowering.py
@@ -7,9 +7,10 @@ Created on 11/10/2018
 
 # import the modules necessary to initiate the L-systems
 import time
-import math
+
 import re
 from openalea.lpy import *
+import math
 from openalea.plantgl.all import *
 from alinea.caribu.CaribuScene import CaribuScene
 import multiprocessing
@@ -22,7 +23,7 @@ from openalea.lpy import Lsystem
 import itertools
 import pandas as pd
 
-path_ = r'D:\Simon\Python\lgrass\lgrass'
+#path_ = r'D:\Simon\Python\lgrass\lgrass'
 
 INPUTS_DIRPATH = 'inputs'
 OUTPUTS_DIRPATH = r'outputs'
@@ -33,7 +34,9 @@ OUTPUTS_DIRPATH = r'outputs'
 
 
 # path_param = os.path.join(INPUTS_DIRPATH,'Parametre_plante_Lgrass.xls') # Fichier contenant les parametres
-path_param = 'D:\Simon\Python\lgrass\lgrass\inputs\Parametre_plante_Lgrass.xls'
+#path_param = 'D:\Simon\Python\lgrass\lgrass\inputs\Parametre_plante_Lgrass.xls'
+path_param = 'inputs/Parametre_plante_Lgrass.xls'
+
 onglet1 = 'FL'
 onglet2 = 'FC'
 TableParamP1 = pd.read_excel(path_param, sheetname=onglet1)
@@ -56,7 +59,7 @@ photoperiod_max_list = np.arange(16, 17, 10)
 max_photo_ind_rate_list = np.arange(1, 2, 10)
 coeff_primordia_emission_vegetative_list = np.arange(1, 2, 10)
 coeff_primordia_emission_reproductive_list = np.arange(1, 6, 10)
-combination_site_sowing_date = np.array([("treatment_4", "2019_01_17")])
+combination_site_sowing_date = np.array([("treatment_1", "2018_11_13")])
 # combination_site_sowing_date = np.array([("LUSIGNAN", "2017_10_15")])
 # combination_site_sowing_date = np.array([("LUSIGNAN", "2012_10_15"),
 #                                          ("LUSIGNAN", "2017_10_15"),
@@ -66,7 +69,7 @@ combination_site_sowing_date = np.array([("treatment_4", "2019_01_17")])
 #                                          ("PLOUDANIEL", "2007_10_15"),
 #                                          ("PLOUDANIEL", "2012_10_15"),
 #                                          ("PLOUDANIEL", "2017_10_15")])
-value_C = np.array([0.7, 1.2])
+#value_C = np.array([0.7, 1.2])
 Premiecroiss = np.array([90, 110, 150, 170])
 
 # column_names = ["name", "temp_vern_min", "temp_vern_inter", "temp_vern_max", "daily_vern_rate",
@@ -79,6 +82,7 @@ Premiecroiss = np.array([90, 110, 150, 170])
 
 #simul_conditions = pd.read_csv(r"D:\Simon\Python\lgrass\lgrass\inputs\plan_simulation.csv")
 simul_conditions = pd.read_csv(os.path.join("inputs/plan_simulation.csv"))
+value_C = pd.read_csv(os.path.join("inputs/donnees_C.csv"))
 
 for x in range(simul_conditions.shape[0]):
     row = simul_conditions.iloc[x]
@@ -101,6 +105,7 @@ for x in range(simul_conditions.shape[0]):
     names.append(name)
     lpy_filename = os.path.join('lgrass.lpy')
     testsim[name] = Lsystem(lpy_filename)
+    #testsim[name].axiom = "CouvertVegetal(2)"
     testsim[name].derivationLength = int(row["derivationLength"])
     testsim[name].option_tallage = row["option_tallage"]
     testsim[name].meteo_path = os.path.join(row["meteo_path"])
@@ -112,8 +117,20 @@ for x in range(simul_conditions.shape[0]):
     testsim[name].cutting_dates = [] if pd.isna(row["cutting_dates"]) \
         else [row["cutting_dates"]] if isinstance(row["cutting_dates"], int) \
         else [int(i) for i in row["cutting_dates"].split("_")]
-    testsim[name].ParamP[0]["C"] = row["value_C"]
-    testsim[name].ParamP[0]["Premiecroiss"] = row["Premiecroiss"]
+    testsim[name].ParamP = []
+    testsim[name].ParamP.append(paramP1)
+    testsim[name].ParamP.append(paramP2)
+    testsim[name].nb_plantes = len(testsim[name].ParamP)
+    testsim[name].NBlignes = int(math.ceil(np.sqrt(testsim[name].nb_plantes)))
+    testsim[name].NBcolonnes = int(math.floor(np.sqrt(testsim[name].nb_plantes)))
+    testsim[name].posPlante = [[i, j] for i, j in zip(sorted(range(testsim[name].NBlignes) * testsim[name].NBcolonnes), range(testsim[name].NBcolonnes) * testsim[name].NBlignes)]
+    # Creation des matrices d'identifiant des plantes et de leur genotype
+    testsim[name].Plantes = np.arange(testsim[name].nb_plantes).reshape(testsim[name].NBlignes, testsim[name].NBcolonnes)
+    testsim[name].Genotypes = np.array([i for i in value_C['geno']]).reshape(testsim[name].NBlignes, testsim[name].NBcolonnes)
+    for i in range(len(value_C["geno"])):
+        testsim[name].ParamP[i]["genotype"] = value_C['geno'].iloc[i]
+        testsim[name].ParamP[i]["C"] = value_C['C'].iloc[i]
+        testsim[name].ParamP[i]["Premiecroiss"] = row["Premiecroiss"]
 
 
 # for x in itertools.product(temp_vern_min_list, temp_vern_inter_list, temp_vern_max_list, daily_vern_rate_list,
@@ -168,10 +185,10 @@ def runlsystem(n):
 
 
 for i in range(simul_conditions.shape[0]):
-    try:
-        runlsystem(i)
-    except:
-        print(simul_conditions.iloc[i]["name"] + " " + "failed")
+    #try:
+    runlsystem(i)
+    #except:
+    #    print(simul_conditions.iloc[i]["name"] + " " + "failed")
 
 
 # for i in range(nb_simul):
