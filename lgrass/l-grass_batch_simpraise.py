fd7ea83463259176b9ce4c82487c16ff0cb61f59
Thibault <thibault.raquet@inrae.fr>
2020-03-19 11:18:03 +0100

    adding module caribu and a new batch for simpraise containing a first version of a new generation of plants by using spikelets.

--- lgrass/l-grass_batch_simpraise.py
+++ lgrass/l-grass_batch_simpraise.py
@@ -1,13 +1,12 @@
 # '''
-# Created on 11/10/2018
+# Created on 19/03/2020
 #
-# @author: modelisation - SR
+# @author: modelisation - TR
 # '''
-# batch pour L-grass with flowering
+
 
 # import the modules necessary to initiate the L-systems
 import time
-
 import re
 from openalea.lpy import *
 import math
@@ -27,19 +26,6 @@ import pandas as pd
 import random
 
 
-# path_ = r'D:\Simon\Python\lgrass\lgrass'
-
-INPUTS_DIRPATH = 'inputs'
-OUTPUTS_DIRPATH = r'outputs'
-# date_string = datetime.datetime.now() .strftime('%Y_%m_%d_%Hh%M')
-# output_batch_path = open(os.path.join(OUTPUTS_DIRPATH, 'output_batch_path', date_string), "wb")
-# output_batch = csv.writer(output_batch_path)
-# output_batch.writerow(['vai', 'vbee', 'sldl', 'absolute_max_leaf_number', 'absolute_min_leaf_number'])
-
-
-# path_param = os.path.join(INPUTS_DIRPATH,'Parametre_plante_Lgrass.xls') # Fichier contenant les parametres
-# path_param = 'D:\Simon\Python\lgrass\lgrass\inputs\Parametre_plante_Lgrass.xls'
-
 TableParamP = pd.read_excel('inputs/Parametre_plante_Lgrass.xls', sheet_name='ParamP')
 simul_conditions = pd.read_csv(os.path.join("inputs/plan_simulation.csv"))
 value_C = pd.read_csv(os.path.join("inputs/donnees_C.csv"))
@@ -47,49 +33,25 @@ param_name = list(TableParamP['name'])
 param_value = list(TableParamP['value'])
 nb_graines_par_epillet = 3
 
-# dict(zip(TableParamP['name'], TableParamP['value']))
-
-# cree la liste de L-systems et liste des noms
+# Cree la liste de L-systems et liste des noms
 testsim = {}
 names = []
 
-
-combination_site_sowing_date = np.array([("treatment_1", "2018_11_13")])
-# combination_site_sowing_date = np.array([("LUSIGNAN", "2017_10_15")])
-# combination_site_sowing_date = np.array([("LUSIGNAN", "2012_10_15"),
-#                                          ("LUSIGNAN", "2017_10_15"),
-#                                          ("THEIX", "2007_10_15"),
-#                                          ("THEIX", "2012_10_15"),
-#                                          ("THEIX", "2017_10_15"),
-#                                          ("PLOUDANIEL", "2007_10_15"),
-#                                          ("PLOUDANIEL", "2012_10_15"),
-#                                          ("PLOUDANIEL", "2017_10_15")])
-# value_C = np.array([0.7, 1.2])
-Premiecroiss = np.array([90, 110, 150, 170])
-
-
-# column_names = ["name", "temp_vern_min", "temp_vern_inter", "temp_vern_max", "daily_vern_rate",
-#                 "basic_vern_rate", "photoperiod_min", "photoperiod_max", "max_photo_ind_rate",
-#                 "coeff_primordia_emission_vegetative", "coeff_primordia_emission_reproductive",
-#                 "derivationLength", "option_tallage", "meteo_path", "sowing_date", "site",
-#                 "output_induction_file_name", "cutting_dates", "value_C", "Premiecroiss]
-
-# simul_conditions = pd.DataFrame(columns=column_names)
-
-# simul_conditions = pd.read_csv(r"D:\Simon\Python\lgrass\lgrass\inputs\plan_simulation.csv")
-
-def runlsystem(opt_caribu):
+def runlsystem(opt_caribu, opt_reproduction):
     for x in range(simul_conditions.shape[0]):
+# Gestion de la recuperation des variables par le lsystem
         row = simul_conditions.iloc[x]
         name = str(row["name"])
         print(name)
         names.append(name)
         lpy_filename = os.path.join('lgrass.lpy')
         testsim[name] = Lsystem(lpy_filename)
-        # ecriture du fichier des parametres d'entree
+
+# Ecriture du fichier des parametres de plante d'entree
         path_param = 'inputs/' + row[0] + '.csv'
         param_init = open(path_param, 'w')
-        # ecriture des parametres variables
+
+# Ecriture des parametres de plante variables
         Geno = []
         C = []
         Geno.append(param_name[0])
@@ -99,7 +61,7 @@ def runlsystem(opt_caribu):
             C.append(str(value_C['C'].iloc[geno]))
         param_init.write(";".join(Geno) + "\n")
         param_init.write(";".join(C) + "\n")
-        # ecriture des parametres constants
+# Ecriture des parametres de plante constants
         for par in range(2, len(param_name)):
             L = []
             L.append(param_name[par])
@@ -107,7 +69,8 @@ def runlsystem(opt_caribu):
                 L.append(str(param_value[par]))
             param_init.write(";".join(L) + "\n")
         param_init.close()
-        # lecture du fichier, creation de ParamP et des parametres de floraison
+
+# lecture du fichier, creation de ParamP et des parametres de floraison
         param_plante = pd.read_csv(os.path.join(path_param), sep=";", header=None)
         testsim[name].ParamP = []
         flowering_param = flowering_functions.FloweringFunctions()
@@ -124,7 +87,8 @@ def runlsystem(opt_caribu):
             flowering_param.param.max_photo_ind_rate.append(L["max_photo_ind_rate"])
             flowering_param.param.coeff_primordia_emission_vegetative.append(L["coeff_primordia_emission_vegetative"])
             flowering_param.param.coeff_primordia_emission_reproductive.append(L["coeff_primordia_emission_reproductive"])
-        # Creation des matrices d'identifiant des plantes et de leur genotype
+
+# Creation des matrices d'identifiant des plantes et de leur genotype
         testsim[name].nb_plantes = len(testsim[name].ParamP)
         testsim[name].NBlignes = int(math.ceil(np.sqrt(testsim[name].nb_plantes)))
         testsim[name].NBcolonnes = int(math.floor(np.sqrt(testsim[name].nb_plantes)))
@@ -135,13 +99,13 @@ def runlsystem(opt_caribu):
                                                                             testsim[name].NBcolonnes)
         testsim[name].Genotypes = np.array([i for i in value_C['geno']]).reshape(testsim[name].NBlignes,
                                                                                  testsim[name].NBcolonnes)
-
-        # Donnees meteo
+# Donnees meteo
         sowing_date = "2018_11_13"
         site = 'treatment_1'
         testsim[name].meteo = meteo_ephem.import_meteo_data('inputs/meteo_controlled_conditions.csv', sowing_date,site)
-        # Gestion caribu
-        if (opt_caribu == 'On'):
+
+# Gestion caribu
+        if (opt_caribu == True):
             dico_caribu = {}
             dico_caribu['azimuths'] = 4
             dico_caribu['zeniths'] = 5
@@ -155,8 +119,8 @@ def runlsystem(opt_caribu):
             dico_caribu['meteo'] = testsim[name].meteo
             dico_caribu['option_tiller_regression'] = testsim[name].option_tiller_regression
             dico_caribu['option_mophogenetic_regulation_by_carbone'] = testsim[name].option_mophogenetic_regulation_by_carbone
-        # Parametres de simulation
-        # testsim[name].axiom = "CouvertVegetal(2)"
+
+# Parametres de simulation
         testsim[name].derivationLength = int(row["derivationLength"])
         testsim[name].option_tallage = row["option_tallage"]
         testsim[name].meteo_path = os.path.join(row["meteo_path"])
@@ -168,7 +132,12 @@ def runlsystem(opt_caribu):
         testsim[name].cutting_dates = [] if pd.isna(row["cutting_dates"]) \
             else [row["cutting_dates"]] if isinstance(row["cutting_dates"], int) \
             else [int(i) for i in row["cutting_dates"].split("_")]
-        # Lancement du lsystem
+
+# Gestion de la creation d une nouvelle generation
+        if(opt_reproduction == True):
+            Epillets = []
+
+# Lancement du lsystem
         lstring = testsim[name].axiom
         for dd in range(testsim[name].derivationLength):
             try :
@@ -184,72 +153,29 @@ def runlsystem(opt_caribu):
                                                                     testsim[name].nb_plantes, dico_caribu, day)
             Viewer.display(lscene)
 
-        # testsim[names[n]].derive() # permet le declenchement de la fonction "End" du script lpy
-        # testsim[names[x]].animate()
-        # print(testsim[names[n]].output_dict)
-        # with open(os.path.join(OUTPUTS_DIRPATH, 'sortie_test_path', str(n) + '.csv'), 'wb') as sortie_test_path:
-        #     sortie_test = csv.writer(sortie_test_path)
-        #     sortie_test.writerows(testsim[names[n]].output_dict.items())
+#Gestion des graines
+        for mod in lstring :
+            if mod.name in ('apex',):
+                if(mod[0].phenological_state == 'reproductive' and mod[0].final_spikelet_number != None):
+                    Epillets.append((mod[0].id_plante, mod[0].id_talle, mod[0].final_spikelet_number))
+        nb_tot_graines = []
+        for tupple in Epillets :
+            # bool = True
+            # for id_epillet in nb_tot_epillets :
+            #     if (tupple == id_epillet):
+            #         bool = False
+            #     break
+            # if(bool == True):
+            #     nb_tot_epillets.append(tupple)
+            for i in range(int(tupple[2])*nb_graines_par_epillet):
+                nb_tot_graines.append(tupple)
+        print(nb_tot_graines)
+        Graines = []
+        for rand in range(testsim[name].nb_plantes):
+            Graines.append(nb_tot_graines[random.randint(0,len(nb_tot_graines)-1)])
+        print(Graines)
+
         testsim[names[x]].clear()
         print(''.join((names[x], " - done")))
 
-        # print(testsim[names[n]].output_dict)
-
-# try:
-runlsystem('Off')
-# except:
-#    print(simul_conditions.iloc[i]["name"] + " " + "failed")
-
-
-# for x in itertools.product(temp_vern_min_list, temp_vern_inter_list, temp_vern_max_list, daily_vern_rate_list,
-#                            basic_vern_rate_list, photoperiod_min_list, photoperiod_max_list, max_photo_ind_rate_list,
-#                            coeff_primordia_emission_vegetative_list, coeff_primordia_emission_reproductive_list,
-#                            combination_site_sowing_date, value_C, Premiecroiss):
-#     print(x)
-#
-#     nb_simul += 1
-#     flowering_param = flowering_functions.FloweringFunctions()
-#     flowering_param.param.temp_vern_min = x[0]
-#     flowering_param.param.temp_vern_inter = x[1]
-#     flowering_param.param.temp_vern_max = x[2]
-#     flowering_param.param.daily_vern_rate = x[3]
-#     flowering_param.param.basic_vern_rate = x[4]
-#     flowering_param.param.photoperiod_min = x[5]
-#     flowering_param.param.photoperiod_max = x[6]
-#     flowering_param.param.max_photo_ind_rate = x[7]
-#     flowering_param.param.coeff_primordia_emission_vegetative = x[8]
-#     flowering_param.param.coeff_primordia_emission_reproductive = x[9]
-#
-#     name = str(x[10][0]) + "_" + str(x[10][1]) + "_C_" + str(x[11]) + "_" + str(x[12])
-#     print(name)
-#     names.append(name)
-#     lpy_filename = os.path.join('lgrass.lpy')
-#     testsim[name] = Lsystem(lpy_filename)
-#     testsim[name].derivationLength = 3000
-#     testsim[name].option_tallage = False
-#     testsim[name].meteo_path = os.path.join('inputs/meteo_controlled_conditions_bloc_1.csv')
-#     testsim[name].sowing_date = x[10][1]
-#     testsim[name].site = x[10][0]
-#     testsim[name].flowering_model = flowering_param
-#     testsim[name].output_induction_file_name = 'induction_' + name
-#     testsim[name].output_organ_lengths_file_name = 'organ_lengths_' + name
-#     testsim[name].cutting_dates = []
-#     testsim[name].ParamP[0]["C"] = x[11]
-#     testsim[name].ParamP[0]["Premiecroiss"] = x[12]
-
-#    simul_conditions = simul_conditions.append(pd.DataFrame([a, ], columns=column_names))
-
-
-# for i in range(nb_simul):
-#     runlsystem(i)
-
-# #run the L-systems
-# if __name__ == '__main__':
-#     multiprocessing.freeze_support()
-#     CPUnb = multiprocessing.cpu_count()-1 #nombre de processeurs, moins un par prudence. (et pour pouvoir faire d'autres choses en meme temps)
-#     print 'nb CPU: ' + str(CPUnb)
-#     pool = multiprocessing.Pool(processes=CPUnb)
-#     for i in range(int(nb_simul)):
-#         pool.apply_async(runlsystem, args=(i,)) #Lance CPUnb simulations en meme temps, lorsqu'une simulation se termine elle est immediatement remplacee par la suivante
-#     pool.close()
-#     pool.join()
+runlsystem(opt_caribu = False, opt_reproduction = True)
