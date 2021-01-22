da344be01b114cdbe84b6f7cf1e61eaa6f3ce01f
srouet <simon.rouet@inra.fr>
2021-01-22 15:53:38 +0100

    Fix vernalization function

--- lgrass/flowering_functions.py
+++ lgrass/flowering_functions.py
@@ -13,12 +13,13 @@ class FloweringFunctions:
         :return:
         """
         if self.param.temp_vern_min <= temperature <= self.param.temp_vern_inter:
-            primary_induction_increment = self.param.daily_vern_rate * temperature + self.param.basic_vern_rate
+            primary_induction_increment = (self.param.daily_vern_rate /
+                                           (self.param.temp_vern_inter - self.param.temp_vern_min)) * \
+                                          (temperature - self.param.temp_vern_min)
         elif self.param.temp_vern_inter < temperature <= self.param.temp_vern_max:
-            # primary_induction_increment = (self.param.daily_vern_rate * temperature + self.param.basic_vern_rate) * (self.param.temp_vern_max - temperature) / (self.param.temp_vern_max - self.param.temp_vern_inter)
-            primary_induction_increment = (-temperature + self.param.temp_vern_max) * (
-                        self.param.daily_vern_rate * self.param.temp_vern_inter + self.param.basic_vern_rate) / (
-                                                      self.param.temp_vern_max - self.param.temp_vern_inter)
+            primary_induction_increment = (self.param.daily_vern_rate /
+                                           (self.param.temp_vern_inter - self.param.temp_vern_max)) * \
+                                          (temperature - self.param.temp_vern_max)
         else:
             primary_induction_increment = 0
         return primary_induction_increment
--- lgrass/parameters.py
+++ lgrass/parameters.py
@@ -5,7 +5,6 @@ class ParametersValues:
         self.temp_vern_inter = 8  # intermediate vernalisation temperature (°C)
         self.temp_vern_max = 17  # maximal vernalisation temperature (°C)
         self.daily_vern_rate = 0.001  # daily vernalisation rate (°C^-1)
-        self.basic_vern_rate = 0.01  # basic vernalisation rate
         self.photoperiod_min = 10.  # minimal efficient photoperiod (h)
         self.photoperiod_max = 16.  # maximal efficient photoperiod (h)
         self.max_photo_ind_rate = 1  # photoperiodic induction ratedaily_vern_rate
